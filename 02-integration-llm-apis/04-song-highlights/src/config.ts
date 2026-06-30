3059d77131a81b365764f6ebc9396e1c8f7e3b68
André Coelho <andrevrcoelho@hotmail.com>
2026-06-30 12:10:52 -0300

    chore: update model name

--- 02-integration-llm-apis/04-song-highlights/src/config.ts
+++ 02-integration-llm-apis/04-song-highlights/src/config.ts
@@ -31,7 +31,7 @@ export const config: ModelConfig = {
   models: [
     // 'qwen/qwen3-coder-next',
     // https://openrouter.ai/models?fmt=cards&max_price=0&order=throughput-high-to-low&supported_parameters=structured_outputs%2Cresponse_format
-    "arcee-ai/trinity-large-preview:free",
+    "openrouter/free",
     // 'gpt-oss-120b:free',
   ],
   provider: {
